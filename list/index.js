105542f79e75b2092770db925809836f76251904
Akira Kusumoto <akirakusumo10@gmail.com>
2013-01-14 09:48:36 +0900

    list for loop

---
+++ list/index.js
@@ -0,0 +1,14 @@
+var mainCtrl = function ($scope) {
+    $scope.users = [
+        {"name": "taguchi", "score": 52.22},
+        {"name": "tanaka", "score": 38.22},
+        {"name": "yamada", "score": 11.11},
+        {"name": "hayashi", "score": 5.25},
+        {"name": "tanahashi", "score": 82.4},
+        {"name": "yasuda", "score": 55.21},
+        {"name": "minami", "score": 32.8},
+        {"name": "yanagi", "score": 72.2}
+    ];
+};
+
+
